70229e8bd3000139787e9aedf3b737bf2e6debea
Ben Steiger <rkrkakak456@gmail.com>
2019-06-19 12:04:18 +0900

    Add project file
    지금까지 만들어논것!

---
+++ Form1.Designer.cs
@@ -0,0 +1,145 @@
+﻿namespace end_cs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.kor_button = new System.Windows.Forms.RadioButton();
+            this.eng_button = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Silver;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 58);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(681, 296);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragDrop);
+            this.pictureBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragEnter);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 362);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(681, 206);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(488, 9);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 41);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "txt파일로 저장하기";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(315, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(143, 41);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "변환하기";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("DIN", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(56, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Language";
+            // 
+            // kor_button
+            // 
+            this.kor_button.AutoSize = true;
+            this.kor_button.Location = new System.Drawing.Point(166, 21);
+            this.kor_button.Name = "kor_button";
+            this.kor_button.Size = new System.Drawing.Size(42, 16);
+            this.kor_button.TabIndex = 8;
+            this.kor_button.Text = "Kor";
+            this.kor_button.UseVisualStyleBackColor = true;
+            // 
+            // eng_button
+            // 
+            this.eng_button.AutoSize = true;
+            this.eng_button.Location = new System.Drawing.Point(236, 21);
+            this.eng_button.Name = "eng_button";
+            this.eng_button.Size = new System.Drawing.Size(45, 16);
+            this.eng_button.TabIndex = 9;
+            this.eng_button.TabStop = true;
+            this.eng_button.Text = "Eng";
+            this.eng_button.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(705, 581);
+            this.Controls.Add(this.eng_button);
+            this.Controls.Add(this.kor_button);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "OCR";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton kor_button;
+        private System.Windows.Forms.RadioButton eng_button;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using MetroFramework.Forms;
+
+using Tesseract;
+using Emgu.CV.Structure;
+using Emgu.CV;
+
+namespace end_cs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.AllowDrop = true;
+        }
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {//다이얼로그 생성 이미지 삽입
+            string image_file = string.Empty;
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.InitialDirectory = @"C:\Users\Ant\Desktop:\";
+            
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                image_file = dialog.FileName;
+            }
+            else if (dialog.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+            pictureBox1.Image = Bitmap.FromFile(image_file);
+        }
+
+        Image img;
+        private void pictureBox1_DragDrop(object sender, DragEventArgs e)
+        {//Occurs when a drag-and - drop operation is completed.
+         //끌어놓기 작업이 완료될때 발생
+            foreach (string pic in ((string[])e.Data.GetData(DataFormats.FileDrop)))
+            {
+                img = Image.FromFile(pic);
+                pictureBox1.Image = img;
+            }
+        }//end DragDrop
+
+        private void pictureBox1_DragEnter(object sender, DragEventArgs e)
+        {//Occurs when an object is dragged into the control's bounds.
+         //마우스로 항목을 끌어 이컨트롤 클라이언트 영역으로 가져올떄 발생
+            e.Effect = DragDropEffects.Copy;
+        }//end DragEnter
+
+        private void button2_Click(object sender, EventArgs e)
+        {//저장버튼
+            string folder = Application.StartupPath + @"\output";
+            DirectoryInfo dir = new DirectoryInfo(folder);
+            if (!dir.Exists) dir.Create();
+
+            string txtFile = folder + @"\output.txt";
+            FileStream filestream = new FileStream(txtFile, FileMode.Append, FileAccess.Write);
+            StreamWriter streamWriter = new StreamWriter(filestream, System.Text.Encoding.Default);
+
+            streamWriter.WriteLine(String.Format(DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss")));
+            streamWriter.WriteLine(pictureBox1.Image);
+            streamWriter.Flush();
+            streamWriter.Close();
+            filestream.Close();
+            
+        }//end 저장버튼클릭
+        private void button3_Click_1(object sender, EventArgs e)
+        {//변환버튼
+            textBox1.ResetText();
+
+            if (kor_button.Checked)
+            {
+                if(pictureBox1.Image == null)
+                {
+                    MessageBox.Show("이미지를 넣으세요~");
+                }//end picturebox null
+                else
+                {
+                    Bitmap img = new Bitmap(pictureBox1.Image);
+                    var ocr = new TesseractEngine("./tessdata", "kor", EngineMode.Default);
+                    var texts = ocr.Process(img);
+                    //MessageBox.Show(texts.GetText());
+                    textBox1.AppendText(texts.GetText());
+                }
+                
+            }//end kor check
+            else if(eng_button.Checked)
+            {
+                if (pictureBox1.Image == null)
+                {
+                    MessageBox.Show("이미지를 넣으세요~");
+                }
+                else
+                {
+                    Bitmap img = new Bitmap(pictureBox1.Image);
+                    var ocr = new TesseractEngine("./tessdata", "eng", EngineMode.TesseractAndCube);
+                    var texts = ocr.Process(img);
+                    //MessageBox.Show(texts.GetText());
+                    textBox1.AppendText(texts.GetText());
+                }
+            }//end eng check
+            else
+            {
+                MessageBox.Show("언어를 선택하세요~");
+            }
+        }//end 변환버튼클릭
+        
+    }
+}
